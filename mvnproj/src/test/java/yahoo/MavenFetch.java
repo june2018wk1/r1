774594bfee6ed5e64e53c387a32c61e0675f346b
Saurav <Saurav@Saurav-PC>
2018-06-17 10:17:00 +0530

    new change

--- mvnproj/src/test/java/yahoo/MavenFetch.java
+++ mvnproj/src/test/java/yahoo/MavenFetch.java
@@ -7,12 +7,9 @@ public class MavenFetch {
 		System.out.println("new line updated");
 		System.out.println("4th line");
 		System.out.println("Conflict");
-<<<<<<< HEAD
-System.out.println("hjjh4566565656");
-=======
+		System.out.println("hjjh4566565656");
 		System.out.println("gdfgdfgdfgdfgdfgdfgdfg");
-
->>>>>>> branch 'master' of https://github.com/june2018wk1/r1.git
+		System.out.println("New Change");
 
 	}
 
